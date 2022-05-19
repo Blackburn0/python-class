8099343e90853dffdb031cce7c1371add84bb4a4
Blackburn0 <99877794+Blackburn0@users.noreply.github.com>
2022-05-19 15:04:55 +0100

    Add files via upload

---
+++ Changing-letter-case.py
@@ -0,0 +1,10 @@
+my_name = input("Enter your fullname:")
+
+# printing of lowercase
+print("Welcome: "+ my_name.lower())
+
+# printing of UPPERCASE
+print("Welcome: "+ my_name.upper())
+
+# printing of Titlecase
+print("Welcome: "+ my_name.title())
